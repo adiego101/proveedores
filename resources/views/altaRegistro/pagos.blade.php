dedf2f12465cf17d5451372dc8dde62c3440eef0
Andrea Lopez <landreaceci17@gmail.com>
2021-11-12 09:29:05 -0300

    cambios en interfaces infoImpositiva y pagos

--- resources/views/altaRegistro/pagos.blade.php
+++ resources/views/altaRegistro/pagos.blade.php
@@ -3,6 +3,7 @@
     <br>
     <label for="tipo-proveedor">Tipo de Proovedor:</label><br>
     <div class="container">
+        <!-- CAMBIO FORMA DE UTILIZAR CHECKBOX
         <div class="row">
             <div class="col-sm">
                 <input type="checkbox" id="prov-estado" value="prov-estado">
@@ -18,12 +19,37 @@
                 <label for="prov-provincial">Proveedor Petrolero</label><br>
             </div>
         </div>
-    </div>
+        -->
+        <div class="form-check">
+            <div class="row">
+                <div class="col col-md-6">
+                    <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]" value="PROVEEDOR DEL ESTADO">PROVEEDOR DEL ESTADO<br>
+                    <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]" value="PROVEEDOR MINERO">PROVEEDOR MINERO<br>
+                    <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]" value="PROVEEDOR PETROLERO">PROVEEDOR PETROLERO<br>
+                    <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]" value="PRODUCTOR PROVINCIAL">PRODUCTOR PROVINCIAL<br>
+                </div>
+            </div>
+        </div>
     <br>
+    <!-- CAMBIO Registro de Pagos de inscripción y renovación: POR UN TITULO EN LUGAR DE UN CAMPO DE TEXTO
     <label for="registro-pagos-inscripccion">Registro de Pagos de inscripción y renovación:</label><br>
     <input type="text" class="form-control" aria-describedby="basic-addon1" id="registro-pagos-inscripccion"
         name="registro-pagos-inscripccion" required><br>
     <hr>
+    -->
+    <h5>Registro de Pagos de inscripción y renovación:</h5><br>
+
+    <label for="fecha-pago">Fecha:</label><br>
+    <input type="text" class="form-control" placeholder="Ingrese la fecha en la que se realizó el pago" aria-describedby="basic-addon1" id="fecha-pago" name="fecha-pago"
+        required><br>
+
+    <label for="importe-pago">Importe:</label><br>
+    <input type="text" class="form-control" placeholder="Ingrese el importe pagado" aria-describedby="basic-addon1" id="importe-pago" name="importe-pago"
+        required><br>
+
+    <label for="observaciones-pago">Observaciones:</label><br>
+    <input type="text" class="form-control" placeholder="Ingrese las observaciones del pago" aria-describedby="basic-addon1" id="observaciones-pago" name="observaciones-pago"
+        required><br>
 
     <h5>Completar solo si desea Registrar Baja del registro:</h5><br>
 
