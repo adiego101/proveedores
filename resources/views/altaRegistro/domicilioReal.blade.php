ce60a97585ff4f50d75b7a76342509ee7b197ec4
nsartini66 <nsartini66@gmail.com>
2021-12-01 14:45:15 -0300

    campo Telefono dinamico en domicilio real - alta

--- resources/views/altaRegistro/domicilioReal.blade.php
+++ resources/views/altaRegistro/domicilioReal.blade.php
@@ -1,10 +1,10 @@
 <fieldset>
 
     <h1>Datos del domicilio real</h1><br>
-
+  
     <div class="container">
         <div class="row">
-            <div class="col-sm">
+            <div class="col-sm field_wrapper">
                 <label for="calle_real">Calle:</label><br>
                 <input type="text" class="form-control" placeholder="Ingrese la calle" aria-describedby="basic-addon1" id="calle_real" name="calle_real"><br>
 
@@ -32,16 +32,13 @@
                 <input type="text" class="form-control" aria-describedby="basic-addon1" id="provincia_real"
                     name="provincia_real"  disabled><br>
 
-                <label for="telefono_real">Teléfono:</label><br>
-                <input type="number" class="form-control" placeholder="Ingrese el número de teléfono"
-                    aria-describedby="basic-addon1" id="telefono_real" name="telefono_real" ><br>
-
                 <label for="email_real">Correo electrónico:</label><br>
                 <input type="email" class="form-control" placeholder="ejemplo@dominio.com"
                     aria-describedby="basic-addon1" id="email_real" name="email_real" ><br>
 
-
-
+                <label for="telefono_real">Teléfono:</label><br>
+                <input type="number" class="form-control" placeholder="Ingrese el número de teléfono" aria-describedby="basic-addon1" id="telefono_real" name="telefono_real[]" >
+                       
             </div>
             <div class="col-sm">
              
@@ -70,12 +67,50 @@
                 <label for="web_real">Página web:</label><br>
                 <input type="text" class="form-control" placeholder="Ingrese la página web"
                     aria-describedby="basic-addon1" id="web_real" name="web_real" ><br>
-
+                    <br>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                    <a href="javascript:void(0);" class="add_button" title="Agregue un nuevo teléfono"><input type="button" value="Agregar Teléfono" class="btn btn-success"></a>
+                </div>
             </div>
         </div>
 
     </div>
-
+    <br>
         <input type="button" name="previous" class="previous btn btn btn-outline-secondary" value="Atrás" />
         <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
+
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var maxField = 3; //Cantidad máxima de campos (teléfonos) a agregar
+            var addButton = $('.add_button');
+            var wrapper = $('.field_wrapper');
+
+            //Nuevo campo html (agregar un nuevo teléfono)
+            var fieldHTML = '<div>'+
+                                '<br>'+
+                                '<label for="telefono_real">Teléfono:</label><br>' +
+                                '<input type="number" class="form-control" aria-describedby="basic-addon1" placeholder="Ingrese el número de teléfono" id="telefono_real" name="telefono_real[]">' + 
+                                '<a href="javascript:void(0);" class="remove_button" title="Elimine el teléfono"><input type="button" value="Eliminar"></a>'+
+                                '<br>'+
+                            '</div>';
+
+
+            var x = 1; //Contador inicial, comienza en 1
+            $(addButton).click(function() {
+                if (x < maxField) { //Verifica el numero maximo de campos a agregar, con el limite establecido
+                    x++; //Incrementa el contador en 1
+                    $(wrapper).append(fieldHTML); // Agrega un nuevo campo html (teléfono)
+                }
+            });
+            $(wrapper).on('click', '.remove_button', function(e) {
+                e.preventDefault();
+                $(this).parent('div').remove(); //Remueve un campo html (teléfono)
+                x--; //Decrementa el contador en 1
+            });
+        });
+    </script>
 </fieldset>
