7ec8b08043284b7fcb93b19a62cbd4cde1b4a740
nsartini66 <nsartini66@gmail.com>
2021-12-15 14:59:13 -0300

    Actualizacion de la interfaz y modal pagos

--- resources/views/altaRegistro/pagos.blade.php
+++ resources/views/altaRegistro/pagos.blade.php
@@ -1,142 +1,137 @@
 <fieldset>
     <h1>Pagos</h1>
     <br>
-    <label for="tipo_proveedor">Tipo de Proovedor:</label><br>
-    {{-- CAMBIO FORMA DE UTILIZAR CHECKBOX
-        <div class="row">
-            <div class="col-sm">
-                <input type="checkbox" id="prov_estado"  value="0"   name="prov_estado">
-                <label for="prov_estado">Proveedor del Estado</label><br>
-                <input type="checkbox" id="prov_minero"  value="0"   name="prov_minero">
-                <label for="prov_minero">Proveedor Minero</label><br>
+    <h4>Tipo de Proveedor:</h4><br>
 
-            </div>
-            <div class="col-sm">
-                <input type="checkbox" id="prov_petrolero"  value="0"   name="prov_petrolero">
-                <label for="prov_petrolero">Proveedor Petrolero</label><br>
-                <input type="checkbox" id="prov_provincial"  value="0"   name="prov_provincial">
-                <label for="prov_provincial">Proveedor Provincial</label><br>
-            </div>
-        </div> --}}
-    <div class="form-check">
-        <div class="row">
-            <div class="col col-md-6">
-                <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]"
-                    value="PROVEEDOR DEL ESTADO">PROVEEDOR DEL ESTADO<br>
-                <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]"
-                    value="PROVEEDOR MINERO">PROVEEDOR MINERO<br>
-                <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]"
-                    value="PROVEEDOR PETROLERO">PROVEEDOR PETROLERO<br>
-                <input class="form-check-input tipo_proveedor" type="checkbox" name="tipo_proveedor[]"
-                    value="PRODUCTOR PROVINCIAL">PRODUCTOR PROVINCIAL<br>
-            </div>
-        </div>
-    </div>
+    <input type="checkbox" id="prov_estado" value="0" name="prov_estado">
+    <label for="prov_estado">Proveedor del Estado</label><br>
+
+    <input type="checkbox" id="prov_minero" value="0" name="prov_minero">
+    <label for="prov_minero">Proveedor Minero</label><br>
+
+    <input type="checkbox" id="prov_petrolero" value="0" name="prov_petrolero">
+    <label for="prov_petrolero">Proveedor Petrolero</label><br>
+
+    <input type="checkbox" id="prov_provincial" value="0" name="prov_provincial">
+    <label for="prov_provincial">Proveedor Provincial</label><br>
+
     <br>
-    <!-- CAMBIO Registro de Pagos de inscripción y renovación: POR UN TITULO EN LUGAR DE UN CAMPO DE TEXTO
-        <label for="registro-pagos-inscripccion">Registro de Pagos de inscripción y renovación:</label><br>
-        <input type="text" class="form-control" aria-describedby="basic-addon1" id="registro-pagos-inscripccion"
-            name="registro-pagos-inscripccion" ><br>
-        <hr>
-        -->
-    <h5>Registro de Pagos de inscripción y renovación:</h5><br>
 
-    <label for="fecha-pago">Fecha:</label><br>
+    <hr>
+
+    <h4>Registro de Pagos de inscripción y renovación:</h4><br>
+
+    <label for="fecha_pago">Fecha:</label><br>
     <input type="date" class="form-control" placeholder="Ingrese la fecha en la que se realizó el pago"
-        aria-describedby="basic-addon1" id="fecha-pago"><br>
+        aria-describedby="basic-addon1" id="fecha_pago"><br>
 
-    <label for="importe-pago">Importe:</label><br>
-    <input type="number" class="form-control" onkeypress="return valideKey(event);" placeholder="Ingrese el importe pagado" aria-describedby="basic-addon1"
-        id="importe-pago"><br>
+    <label for="importe_pago">Importe:</label><br>
+    <input type="number" class="form-control" onkeypress="return valideKey(event);" placeholder="Ingrese el importe pagado" aria-describedby="basic-addon1" id="importe_pago"><br>
 
     <label for="observaciones_pago">Observaciones:</label><br>
     <input type="text" class="form-control" placeholder="Ingrese las observaciones del pago"
         aria-describedby="basic-addon1" id="observaciones_pago"><br>
 
-    <input type="button" value="Agregar" class="btn btn-outline-primary" id="agregar-pago">
-    <div class="table table-bordered">
-        <table id="tabla-pagos">
-            <thead align="center">
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <a id="add_pago" class="btn btn-success">Agregar Pago</a>
+    </div>
+    <br>
+
+    <div>
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <th scope="col">Fecha</th>
-                    <th scope="col">Importe</th>
-                    <th scope="col">Observaciones</th>
-                    <th scope="col">Acción</th>
+                    <th>Fecha</th>
+                    <th>Importe</th>
+                    <th>Observaciones</th>
+                    <th>Acciones</th>
                 </tr>
             </thead>
-            <tbody id="body_pagos_table"></tbody>
+            <tbody id="body_table_pago"></tbody>
         </table>
     </div>
 
     <input type="button" name="previous" class="previous btn btn btn-outline-secondary" value="Atrás" />
-
     <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
+
+    <!--Incluimos el modal para editar los campos de un pago-->
     @include('modales.editarPago')
 
 </fieldset>
+
 @push('js')
 
     <script type="text/javascript">
+
         let fecha;
         let importe;
         let observaciones_pago;
-        $("#agregar-pago").on("click", function(e) {
-            fecha = $('#fecha-pago').val();
-            $('#fecha-pago').val('');
-            importe = $('#importe-pago').val();
-            $('#importe-pago').val('');
-            observaciones_pago = $('#observaciones_pago').val();
-            $('#observaciones_pago').val('');
+        let indice = 1;
 
-            $("#body_pagos_table").append(
-                '<tr id="row' + i + '">' +
-                '<td><input type="date" class="form-control" aria-describedby="basic-addon1" id="fecha' + i +
-                '" name="fechas-pagos[]" readonly value="' + fecha + '"></td>' +
-                '<td><input type="number" class="form-control" aria-describedby="basic-addon1" id="importe' + i +
-                '" name="importes-pagos[]" readonly value="' + importe + '"></td>' +
-                '<td><input type="text" class="form-control" aria-describedby="basic-addon1" id="observaciones' +
-                i + '" name="observaciones-pagos[]" readonly value="' + observaciones_pago + '"></td>' +
-                '<td><button type="button" name="edit" id="' + i +
-                '" class="btn btn-warning btn-sm btn_edit" title="editar pago"><i class="fas fa-edit"></i></button>' +
-                '<button type="button" name="remove" id="' + i +
-                '" class="btn btn-danger btn-sm btn_remove" title="quitar pago"><i class="fas fa-trash"></i></button>' +
+        $("#add_pago").on("click", function(e) {
+
+            fecha = $('#fecha_pago').val();
+            importe = $('#importe_pago').val();
+            observaciones_pago = $('#observaciones_pago').val();
+
+            $("#body_table_pago").append(
+                '<tr id="row_pago' + indice + '">' +
+                '<td><input type="date" class="form-control" aria-describedby="basic-addon1" id="fecha_pago' + indice +
+                '" name="fechas_pagos[]" readonly value="' + fecha + '"></td>' +
+                '<td><input type="number" class="form-control" aria-describedby="basic-addon1" id="importe_pago' + indice +
+                '" name="importes_pagos[]" readonly value="' + importe + '"></td>' +
+                '<td><input type="text" class="form-control" aria-describedby="basic-addon1" id="observaciones_pago' + indice + '" name="observaciones_pagos[]" readonly value="' + observaciones_pago + '"></td>' +
+                '<td><button type="button" name="edit" id="' + indice +
+                '" class="btn btn-warning btn-sm btn_edit_pago" title="editar pago"><indice class="fas fa-edit"></i></button>' +
+                '<button type="button" name="remove" id="' + indice +
+                '" class="btn btn-danger btn-sm btn_remove_pago" title="quitar pago"><indice class="fas fa-trash"></i></button>' +
                 '</td>' +
                 '</tr>'
             );
+
+            indice++;
+
+            //Limpiamos cada campo luego de presionar el botón Agregar Pago
+
+            document.getElementById("fecha_pago").value = "";
+            document.getElementById("importe_pago").value = "";
+            document.getElementById("observaciones_pago").value = "";
+
         });
-            $(document).on("click", ".btn_remove", function() {
-
-                //cuando da click al boton quitar, obtenemos el id del boton
-                let button_id = $(this).attr("id");
-
-                //borra la fila
-                $("#row" + button_id + "").remove();
-            });
 
 
+        $(document).on("click", ".btn_remove_pago", function() {
 
-            //Cargamos los inputs del modal con los datos de la fila de la tabla
+            //cuando da click al boton quitar, obtenemos el id del boton
+            let button_id = $(this).attr("id");
 
-            $(document).on("click", ".btn_edit", function() {
-                //cuando da click al boton editar, obtenemos el id del boton
-                let button_id = $(this).attr("id");
-                //Recuperamos los valores de los campos pertenecientes a una fila
-                let modal_fecha = $("#fecha" + button_id).val();
-                let modal_importe = $("#importe" + button_id).val();
-                let modal_observaciones = $("#observaciones" + button_id).val();
-
-                //Desplegamos el modal
-                $('#editarPagoModal').modal('show');
-
-                //Enviamos los valores recuperados anteriormente a los inputs del modal
-                $('#modal_fecha').val(modal_fecha);
-                $('#modal_importe').val(modal_importe);
-                $('#modal_observaciones').val(modal_observaciones);
-            });
+            //borra la fila
+            $("#row_pago" + button_id + "").remove();
+        });
 
 
+        //Cargamos los inputs del modal con los datos de la fila de la tabla
+
+        $(document).on("click", ".btn_edit_pago", function() {
+
+            //cuando da click al boton editar, obtenemos el id del boton
+            let button_id = $(this).attr("id");
+            //Recuperamos los valores de los campos pertenecientes a una fila
+            let modal_fecha = $("#fecha_pago" + button_id).val();
+            let modal_importe = $("#importe_pago" + button_id).val();
+            let modal_observaciones = $("#observaciones_pago" + button_id).val();
+
+            //Desplegamos el modal
+            $('#modal_pago').modal('show');
+
+            //Enviamos los valores recuperados anteriormente a los inputs del modal
+            $('#modal_fecha').val(modal_fecha);
+            $('#modal_importe').val(modal_importe);
+            $('#modal_observaciones').val(modal_observaciones);
+            $('#numero_fila_pago').val(button_id);
+
+        });
 
     </script>
-
 
 @endpush
--- resources/views/modales/editarPago.blade.php
+++ resources/views/modales/editarPago.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div id="editarPagoModal" class="modal fade" role="dialog">
+<div id="modal_pago" class="modal fade" role="dialog">
     <div class="modal-dialog">
         <!-- Contenido del Modal-->
         <div class="modal-content">
@@ -7,60 +7,57 @@
                 <h1>Editar Pago</h1>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
-    </div>
+            <div class="modal-body">
 
-    <br />
+                <div class="row">
+                    <div class="col-sm">
+                        <label for="modal_fecha">Fecha:</label><br />
+                        <input type="date" class="form-control" placeholder="Fecha" aria-describedby="basic-addon1"
+                            id="modal_fecha" /><br />
 
-    <!-- SE DEBEN ELIMINAR LOS ATRIBUTOS NAME DE CADA CAMPO Y SOLO DEJARLOS EN EL APPEND JS, PORQUE SINO AL ENVIAR LOS DATOS A LA BD SE ALMACENA SIEMPRE UN VALOR NULO AL PRINCIPIO. -->
+                        <label for="modal_importe">Importe:</label><br />
+                        <input type="text" class="form-control" placeholder="Importe" aria-describedby="basic-addon1"
+                            id="modal_importe" /><br />
 
-    <div class="row">
-        <div class="col-sm">
-            <label for="modal_fecha">Fecha:</label><br />
-            <input type="text" class="form-control" placeholder="Fecha" aria-describedby="basic-addon1"
-                id="modal_fecha"/><br />
-
-            <label for="modal_importe">Importe:</label><br />
-            <input type="text" class="form-control" placeholder="Importe" aria-describedby="basic-addon1"
-                id="modal_importe" /><br />
-
-            <label for="modal_observaciones">Observaciones:</label><br />
-            <input type="text" class="form-control" placeholder="Observaciones" aria-describedby="basic-addon1"
-                id="modal_observaciones" /><br />
-        </div>
-
-        <div class="modal-footer">
-            <input id="numero_filasu" name="numero_filasu" type="hidden">
-            <button type="button" name="edit" class="btn btn-success btn_edit_modal">Editar</button>
-            <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Cancelar</button>
+                        <label for="modal_observaciones">Observaciones:</label><br />
+                        <input type="text" class="form-control" placeholder="Observaciones"
+                            aria-describedby="basic-addon1" id="modal_observaciones" /><br />
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input id="numero_fila_pago" name="numero_fila_pago" type="hidden">
+                    <button type="button" name="edit" class="btn btn-success btn_edit_modal">Editar</button>
+                    <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Cancelar</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
-</div>
+
 @push('js')
 
 
-<script type="text/javascript">
+    <script type="text/javascript">
+        //Modificamos los valores actuales, por los nuevos valores ingresados en el modal
 
-    //Modificamos los valores actuales, por los nuevos valores ingresados en el modal
+        $(document).on("click", ".btn_edit_modal", function() {
 
-    $(document).on("click", ".btn_edit_modal", function() {
+            //Obtenemos el numero de la fila que queremos modificar
+            var id_filapago = $("#numero_fila_pago").val();
 
-                //Obtenemos el numero de la fila que queremos modificar
-                var id_filasu = $("#numero_filasu").val();
+            //Recuperamos los valores de los campos del modal
+            var modal_fecha = $("#modal_fecha").val();
+            var modal_importe = $("#modal_importe").val();
+            var modal_observaciones = $("#modal_observaciones").val();
 
-                //Recuperamos los valores de los campos del modal
-                var modal_fecha = $("#modal_fecha").val();
-                var modal_importe = $("#modal_importe").val();
-                var modal_observaciones = $("#modal_observaciones").val();
+            //Ocultamos el modal
+            $('#modal_pago').modal('hide');
 
-                //Ocultamos el modal
-                $('#editarPagoModal').modal('hide');
+            //Enviamos los valores recuperados anteriormente del modal, a los inputs de la tabla
+            $('#fecha_pago' + id_filapago).val(modal_fecha);
+            $('#importe_pago' + id_filapago).val(modal_importe);
+            $('#observaciones_pago' + id_filapago).val(modal_observaciones);
 
-                //Enviamos los valores recuperados anteriormente del modal, a los inputs de la tabla
-                $('#fecha'+id_filasu).val(modal_fecha);
-                $('#importe'+id_filasu).val(modal_importe);
-                $('#observaciones'+id_filasu).val(modal_observaciones);
-
-            });
-</script>
+        });
+    </script>
 @endpush
