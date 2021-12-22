15ce50556128483a435c04de161954e08c4ba612
nsartini66 <nsartini66@gmail.com>
2021-12-22 11:46:15 -0300

    Validaciones en imp ingr brutos e inf. impositiva

--- resources/views/altaRegistro/Impuestosingresosbrutos.blade.php
+++ resources/views/altaRegistro/Impuestosingresosbrutos.blade.php
@@ -5,13 +5,12 @@
   <h4>Ingresos brutos:</h4><br>
 
   <label for="nro_ingresos_brutos">Número de ingresos brutos:</label><br>
-  <input type="number"  onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de ingresos brutos" aria-describedby="basic-addon1" id="nro_ingresos_brutos" name="nro_ingresos_brutos" ><br>
+  <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de ingresos brutos" aria-describedby="basic-addon1" id="nro_ingresos_brutos" name="nro_ingresos_brutos" maxlength="9"><br>
 
   <label for="jurisdiccion">Jurisdicción:</label><br>
-  <input type="text" class="form-control" placeholder="Ingrese su jurisdicción" aria-describedby="basic-addon1" id="jurisdiccion" name="jurisdiccion" ><br>
+  <input type="text" class="form-control" placeholder="Ingrese su jurisdicción" aria-describedby="basic-addon1" id="jurisdiccion" name="jurisdiccion" maxlength="20"><br>
 
-   <!--En este caso, se deben recuperar los datos de la BD -->
-   <label for="tipo_contribuyente">Tipo de contribuyente:</label><br>
+  <label for="tipo_contribuyente">Tipo de contribuyente:</label><br>
   <select class="form-control" aria-describedby="basic-addon1" id="tipo_contribuyente" name="tipo_contribuyente">
     <option selected value="Contribuyente Puro">Contribuyente Puro</option>
     <option value="Contribuyente Convenio Multilateral">Contribuyente Convenio Multilateral</option>
@@ -24,15 +23,14 @@
   <h4>Habilitaciones:</h4><br>
 
   <label for="nro_habilitacion_municipal">Número de habilitación municipal:</label><br>
-  <input type="number"  onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de habilitación municipal" aria-describedby="basic-addon1" id="nro_habilitacion_municipal" name="nro_habilitacion_municipal" ><br>
+  <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de habilitación municipal" aria-describedby="basic-addon1" id="nro_habilitacion_municipal" name="nro_habilitacion_municipal" maxlength="20"><br>
 
   <div class="container">
     <div class="row">
       <div class="col-sm">
-        <!--En este caso, se deben recuperar las provincias de la BD -->
         <label for="provincia_habilitacion">Provincia:</label><br>
         <select class="form-control" aria-describedby="basic-addon1" id="provincia_habilitacion" name="provincia_habilitacion">
-        <option value=" ">Seleccione una provincia</option>
+          <option value=" ">Seleccione una provincia</option>
           @forelse($provincias as $provincia)
             <option value="{{$provincia->nombre_provincia}}">{{$provincia->nombre_provincia}}</option>
           @empty
@@ -42,7 +40,6 @@
         <br>
       </div>
       <div class="col-sm">
-        <!--En este caso, se deben recuperar las localidades de la BD -->
         <label for="localidad_habilitacion">Localidad:</label><br>
         <select class="form-control" aria-describedby="basic-addon1" id="localidad_habilitacion" name="localidad_habilitacion">
           <option value=" ">Seleccione una localidad</option>
@@ -52,75 +49,70 @@
     </div>
 <hr>
     <div class="row">
-
       <div class="col-sm">
         <label for="nro_inscripcion_personas_juridicas">Número de inscripción personas jurídicas:</label><br>
-        <input type="number"  onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de inscripción de personas jurídicas" aria-describedby="basic-addon1" id="nro_inscripcion_personas_juridicas" name="nro_inscripcion_personas_juridicas" ><br>
+        <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de inscripción de personas jurídicas" aria-describedby="basic-addon1" id="nro_inscripcion_personas_juridicas" name="nro_inscripcion_personas_juridicas" maxlength="4"><br>
       </div>
 
       <div class="col-sm">
-        <!--En este caso, se deben recuperar las provincias de la BD -->
         <label for="provincia_inscrip_personas_jur">Provincia:</label><br>
         <select class="form-control" aria-describedby="basic-addon1" id="provincia_inscrip_personas_jur" name="provincia_inscrip_personas_jur">
-        <option value=" ">Seleccione una provincia</option>
-        @foreach($provincias as $provincia)
-          <option value="{{$provincia->nombre_provincia}}">{{$provincia->nombre_provincia}}</option>
-        @endforeach
+          <option value=" ">Seleccione una provincia</option>
+          @forelse($provincias as $provincia)
+            <option value="{{$provincia->nombre_provincia}}">{{$provincia->nombre_provincia}}</option>
+          @empty
+            <option value=" "></option>
+          @endforelse
         </select>
         <br>
       </div>
-
     </div>
 
     <div class="row">
-
       <div class="col-sm">
         <label for="registro_publico_de_comercio">Registro público de comercio:</label><br>
-        <input type="text" class="form-control" placeholder="Ingrese el registro público de comercio" aria-describedby="basic-addon1" id="registro_publico_de_comercio" name="registro_publico_de_comercio" ><br>
+        <input type="text" class="form-control" placeholder="Ingrese el registro público de comercio" aria-describedby="basic-addon1" id="registro_publico_de_comercio" name="registro_publico_de_comercio" maxlength="20"><br>
       </div>
 
       <div class="col-sm">
-        <!--En este caso, se deben recuperar las provincias de la BD -->
         <label for="provincia_registro_publico">Provincia:</label><br>
         <select class="form-control" aria-describedby="basic-addon1" id="provincia_registro_publico" name="provincia_registro_publico">
-        <option value=" ">Seleccione una provincia</option>
-        @foreach($provincias as $provincia)
-          <option value="{{$provincia->nombre_provincia}}">{{$provincia->nombre_provincia}}</option>
-        @endforeach
+          <option value=" ">Seleccione una provincia</option>
+          @forelse($provincias as $provincia)
+            <option value="{{$provincia->nombre_provincia}}">{{$provincia->nombre_provincia}}</option>
+          @empty
+            <option value=" "></option>
+          @endforelse
         </select>
         <br>
       </div>
-
     </div>
 
     <div class="row">
-
       <div class="col-sm">
         <label for="inspeccion_gral_justicia">Inspección general de justicia:</label><br>
-        <input type="text" class="form-control" placeholder="Ingrese la inspección general de justicia" aria-describedby="basic-addon1" id="inspeccion_gral_justicia" name="inspeccion_gral_justicia" ><br>
+        <input type="text" class="form-control" placeholder="Ingrese la inspección general de justicia" aria-describedby="basic-addon1" id="inspeccion_gral_justicia" name="inspeccion_gral_justicia" maxlength="20"><br>
       </div>
 
       <div class="col-sm">
-        <!--En este caso, se deben recuperar las provincias de la BD -->
         <label for="provincia_inspeccion_justicia">Provincia:</label><br>
         <select class="form-control" aria-describedby="basic-addon1" id="provincia_inspeccion_justicia" name="provincia_inspeccion_justicia">
-        <option value=" ">Seleccione una provincia</option>
-        @foreach($provincias as $provincia)
-          <option value="{{$provincia->nombre_provincia}}">{{$provincia->nombre_provincia}}</option>
-        @endforeach
+          <option value=" ">Seleccione una provincia</option>
+          @forelse($provincias as $provincia)
+            <option value="{{$provincia->nombre_provincia}}">{{$provincia->nombre_provincia}}</option>
+          @empty
+            <option value=" "></option>
+          @endforelse
         </select>
         <br>
       </div>
-
     </div>
-
   </div>
 
   <br>
 
   <input type="button" name="previous" class="previous btn btn btn-outline-secondary" value="Atrás" />
   <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
-
 
 
   <script type="text/javascript">
--- resources/views/altaRegistro/infoImpositiva.blade.php
+++ resources/views/altaRegistro/infoImpositiva.blade.php
@@ -1,12 +1,7 @@
-
 <fieldset>
 
   <h1>Información Impositiva</h1><br>
 
-  {{--<label for="cuit">Cuit:</label><br>
-  <input type="text" class="form-control" placeholder="Ingrese el número de cuit de la empresa" aria-describedby="basic-addon1" id="cuit" name="cuit" ><br>
---}}
-  <!--En este caso, se deben recuperar los datos de la BD -->
   <label for="tipo_de_sociedad">Tipo de sociedad:</label><br>
   <select class="form-control" aria-describedby="basic-addon1" id="tipo_de_sociedad" name="tipo_de_sociedad">
     <option selected value="UNIPERSONAL">UNIPERSONAL</option>
@@ -20,8 +15,6 @@
   </select>
   <br>
 
-
-  <!--En este caso, se deben recuperar los datos de la BD -->
   <label for="situacion_iva">Situación IVA:</label><br>
   <select class="form-control" aria-describedby="basic-addon1" id="situacion_iva" name="situacion_iva">
     <option selected value="RESPONSABLE INSCRIPTO">RESPONSABLE INSCRIPTO</option>
@@ -32,9 +25,8 @@
   <br>
 
   <label for="exento_en_cod_de_actividad">Excento en código de actividad:</label><br>
-  <input type="text" class="form-control" aria-describedby="basic-addon1" placeholder="Ingrese el código de la actividad en la que se encuentra excento" id="exento_en_cod_de_actividad" name="exento_en_cod_de_actividad" ><br>
+  <input type="text" class="form-control" aria-describedby="basic-addon1" placeholder="Ingrese el código de la actividad en la que se encuentra excento" id="exento_en_cod_de_actividad" name="exento_en_cod_de_actividad" maxlength="20"><br>
 
-  <!--En este caso, se deben recuperar las provincias de la BD -->
   <label for="en_la_provincia_de">En la provincia de:</label><br>
   <select class="form-control" aria-describedby="basic-addon1" id="en_la_provincia_de" name="en_la_provincia_de">
     <option value=" ">Seleccione una provincia</option>
@@ -57,16 +49,10 @@
       </div>
     </div>
   </div>
-  <!-- NI EN EL FORMULARIO NI EN LA BASE DE DATOS SE SOLICITA UN MOTIVO DE EXCLUSION
-  <label for="motivo-exclusion">Motivo de la exclusión:</label><br>
-  <input type="text" class="form-control" aria-describedby="basic-addon1" placeholder="En caso de corresponder, indique el motivo" id="motivo-exclusion" name="motivo-exclusion"><br>
-  -->
+
   <br>
+
   <input type="button" name="previous" class="previous btn btn btn-outline-secondary" value="Atrás" />
   <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
 
 </fieldset>
-
-
-
-
