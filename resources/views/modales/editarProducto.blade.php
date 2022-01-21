776164dc2fec74dea64cadc5df8c8d706b8cb386
nsartini66 <nsartini66@gmail.com>
2022-01-21 11:56:20 -0300

    Validacion front en modal editar producto

--- resources/views/modales/editarProducto.blade.php
+++ resources/views/modales/editarProducto.blade.php
@@ -9,31 +9,27 @@
                 </div>
                 <div class="modal-body">
 
-                    <div class="row">
-                        <div class="col-sm">
-                            <label for="modal_producto_elaborado">Producto elaborado:</label><br>
-                            <input list="productos" name="modal_producto_elaborado" id="modal_producto_elaborado"  class="form-control" placeholder="Ingrese o seleccione el producto que produce" />
-                            <datalist id="productos">
-                                @foreach($productos as $producto)
-                                <option value="{{$producto->producto_elaborado}}">{{$producto->producto_elaborado}}</option>
-                                @endforeach
-                            </datalist>
-                            <br>
+                    <label for="modal_producto_elaborado">Producto elaborado:</label><br>
+                    <input list="productos" name="modal_producto_elaborado" id="modal_producto_elaborado"  class="form-control" placeholder="Ingrese o seleccione el producto que produce" />
+                    <datalist id="productos">
+                        @forelse($productos as $producto)
+                            <option value="{{$producto->producto_elaborado}}">
+                        @empty
+                            <option value=" ">
+                        @endforelse
+                    </datalist>
+                    <br>
 
-                            <label for="modal_rnpa">RNPA:</label><br>
-                            <input type="text" class="form-control" aria-describedby="basic-addon1" id="modal_rnpa"
-                            name="modal_rnpa" placeholder="Ingrese el RNPA"><br>
-                        </div>
+                    <label for="modal_rnpa">RNPA:</label><br>
+                    <input type="text" class="form-control" aria-describedby="basic-addon1" id="modal_rnpa" name="modal_rnpa" placeholder="Ingrese el RNPA" maxlength="20"><br>
+                 
+                    <label for="modal_unidad_producida">Unidad producida:</label><br>
+                    <input type="text" onkeypress="return valideKey(event);" class="form-control" aria-describedby="basic-addon1" id="modal_unidad_producida"
+                    name="modal_unidad_producida" placeholder="Ingrese la cantidad de unidades producidas" maxlength="9"><br>
 
-                        <div class="col-sm">
-                            <label for="modal_unidad_producida">Unidad producida:</label><br>
-                            <input type="number" class="form-control" aria-describedby="basic-addon1" id="modal_unidad_producida"
-                            name="modal_unidad_producida" placeholder="Ingrese la cantidad de unidades producidas"><br>
-
-                            <label for="modal_produccion_total">Capacidad de producción total:</label><br>
-                            <input type="number" class="form-control" aria-describedby="basic-addon1" id="modal_produccion_total" name="modal_produccion_total" placeholder="Ingrese la producción total"><br>
-                        </div>
-                    </div>
+                    <label for="modal_produccion_total">Capacidad de producción total:</label><br>
+                    <input type="text" onkeypress="return valideKey(event);" class="form-control" aria-describedby="basic-addon1" id="modal_produccion_total" name="modal_produccion_total" placeholder="Ingrese la producción total" maxlength="9"><br>
+                       
                     <div class="modal-footer">
                         <input id="numero_fila_producto" name="numero_fila_producto" type="hidden">
                         <button type="button" name="edit" class="btn btn-success btn_edit_modal">Editar</button>
