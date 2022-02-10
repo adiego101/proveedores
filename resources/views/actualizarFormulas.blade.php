8cc437b0d6bc7bc99ebbcc03129c19ee351720c9
nsartini66 <nsartini66@gmail.com>
2022-02-10 15:12:11 -0300

    Validaciones en panel actualizar formulas

--- resources/views/actualizarFormulas.blade.php
+++ resources/views/actualizarFormulas.blade.php
@@ -27,37 +27,37 @@
             @case ('Facturacion')
             <div class="col-sm">                  
                 <label for="actualizar_facturacion">Facturación:</label><br>
-                <input type="text" class="form-control" placeholder="Ingrese el porcentaje de facturación" aria-describedby="basic-addon1" id="actualizar_facturacion" name="actualizar_facturacion" maxlength="4" value="{{$ponderacion->valor_ponderacion}}"><br>
+                <input type="text" class="form-control" placeholder="Ingrese el porcentaje de facturación" aria-describedby="basic-addon1" id="actualizar_facturacion" name="actualizar_facturacion" maxlength="4" value="{{$ponderacion->valor_ponderacion}}" required><br>
 
             @break
             @case ('Gastos')
 
                 <label for="actualizar_gastos">Gastos:</label><br>
-                <input type="text" class="form-control" placeholder="Ingrese el porcentaje de gastos" aria-describedby="basic-addon1" id="actualizar_gastos" name="actualizar_gastos" maxlength="4" value="{{$ponderacion->valor_ponderacion}}"><br>
+                <input type="text" class="form-control" placeholder="Ingrese el porcentaje de gastos" aria-describedby="basic-addon1" id="actualizar_gastos" name="actualizar_gastos" maxlength="4" value="{{$ponderacion->valor_ponderacion}}" required><br>
                                   
             @break
             @case ('Mano_Obra')
 
                 <label for="actualizar_obra">Mano de Obra:</label><br>
-                <input type="text" class="form-control" placeholder="Ingrese el porcentaje de mano de obra" aria-describedby="basic-addon1" id="actualizar_obra" name="actualizar_obra" maxlength="4" value="{{$ponderacion->valor_ponderacion}}"><br>
+                <input type="text" class="form-control" placeholder="Ingrese el porcentaje de mano de obra" aria-describedby="basic-addon1" id="actualizar_obra" name="actualizar_obra" maxlength="4" value="{{$ponderacion->valor_ponderacion}}" required><br>
             </div>                 
             @break
             @case ('Antiguedad')
             <div class="col-sm">
                 <label for="actualizar_antiguedad">Antiguedad:</label><br>
-                <input type="text" class="form-control" placeholder="Ingrese la antiguedad en Santa Cruz" aria-describedby="basic-addon1" id="actualizar_antiguedad" name="actualizar_antiguedad" maxlength="4" value="{{$ponderacion->valor_ponderacion}}"><br>
+                <input type="text" class="form-control" placeholder="Ingrese la antiguedad en Santa Cruz" aria-describedby="basic-addon1" id="actualizar_antiguedad" name="actualizar_antiguedad" maxlength="4" value="{{$ponderacion->valor_ponderacion}}" required><br>
                                      
             @break
             @case ('Dom_fiscal')
 
                 <label for="actualizar_domicilio">Domicilio Fiscal:</label><br>
-                <input type="text" class="form-control" placeholder="Ingrese el domicilio fiscal" aria-describedby="basic-addon1" id="actualizar_domicilio" name="actualizar_domicilio" maxlength="4" value="{{$ponderacion->valor_ponderacion}}"><br>
+                <input type="text" class="form-control" placeholder="Ingrese el domicilio fiscal" aria-describedby="basic-addon1" id="actualizar_domicilio" name="actualizar_domicilio" maxlength="4" value="{{$ponderacion->valor_ponderacion}}" required><br>
                                       
             @break
             @case ('Valor_Agregado')
 
                 <label for="actualizar_agregado">Valor Agregado:</label><br>
-                <input type="text" class="form-control" placeholder="Ingrese el valor agregado" aria-describedby="basic-addon1" id="actualizar_agregado" name="actualizar_agregado" maxlength="4" value="{{$ponderacion->valor_ponderacion}}"><br>
+                <input type="text" class="form-control" placeholder="Ingrese el valor agregado" aria-describedby="basic-addon1" id="actualizar_agregado" name="actualizar_agregado" maxlength="4" value="{{$ponderacion->valor_ponderacion}}" required><br>
             </div>                  
             @break
         @endswitch
@@ -80,12 +80,12 @@
                 <div class="col-sm">
                   
                     <label for="local_inicial">valor inicial:</label><br>
-                    <input type="text" class="form-control" placeholder="Ingrese el rango del índice local inicial" aria-describedby="basic-addon1" id="local_inicial" name="local_inicial" maxlength="3" value="{{$jerarquia->valor_desde}}"><br>
+                    <input type="number" class="form-control" placeholder="Ingrese el rango del índice local inicial" aria-describedby="basic-addon1" id="local_inicial" name="local_inicial" min="0" max="100" value="{{$jerarquia->valor_desde}}" required><br>
                 </div>
                 
                 <div class="col-sm">
                     <label for="local_final">valor final:</label><br>
-                    <input type="text" class="form-control" placeholder="Ingrese el rango del índice local final" aria-describedby="basic-addon1" id="local_final" name="local_final" maxlength="3" value="{{$jerarquia->valor_hasta}}"><br>
+                    <input type="number" class="form-control" placeholder="Ingrese el rango del índice local final" aria-describedby="basic-addon1" id="local_final" name="local_final" min="0" max="100" value="{{$jerarquia->valor_hasta}}" required><br>
                 </div>
             </div>
                  
@@ -97,12 +97,12 @@
                 <div class="col-sm">
                   
                     <label for="intermedio_inicial">valor inicial:</label><br>
-                    <input type="text" class="form-control" placeholder="Ingrese el rango del índice intermedio inicial" aria-describedby="basic-addon1" id="intermedio_inicial" name="intermedio_inicial" maxlength="3" value="{{$jerarquia->valor_desde}}"> 
+                    <input type="number" class="form-control" placeholder="Ingrese el rango del índice intermedio inicial" aria-describedby="basic-addon1" id="intermedio_inicial" name="intermedio_inicial" min="0" max="100" value="{{$jerarquia->valor_desde}}" required> 
                 </div>
                 
                 <div class="col-sm">
                     <label for="intermedio_final">valor final:</label><br>
-                    <input type="text" class="form-control" placeholder="Ingrese el rango del índice intermedio final" aria-describedby="basic-addon1" id="intermedio_final" name="intermedio_final" maxlength="3" value="{{$jerarquia->valor_hasta}}"> 
+                    <input type="number" class="form-control" placeholder="Ingrese el rango del índice intermedio final" aria-describedby="basic-addon1" id="intermedio_final" name="intermedio_final" min="0" max="100" value="{{$jerarquia->valor_hasta}}" required> 
                 </div>
             </div>
 
@@ -115,12 +115,12 @@
                 <div class="col-sm">
                   
                     <label for="foraneo_inicial">valor inicial:</label><br>
-                    <input type="text" class="form-control" placeholder="Ingrese el rango del índice foráneo inicial" aria-describedby="basic-addon1" id="foraneo_inicial" name="foraneo_inicial" maxlength="3" value="{{$jerarquia->valor_desde}}"><br>
+                    <input type="number" class="form-control" placeholder="Ingrese el rango del índice foráneo inicial" aria-describedby="basic-addon1" id="foraneo_inicial" name="foraneo_inicial" min="0" max="100" value="{{$jerarquia->valor_desde}}" required><br>
                 </div>
                 
                 <div class="col-sm">
                     <label for="foraneo_final">valor final:</label><br>
-                    <input type="text" class="form-control" placeholder="Ingrese el rango del índice foráneo final" aria-describedby="basic-addon1" id="foraneo_final" name="foraneo_final" maxlength="3" value="{{$jerarquia->valor_hasta}}"><br> 
+                    <input type="number" class="form-control" placeholder="Ingrese el rango del índice foráneo final" aria-describedby="basic-addon1" id="foraneo_final" name="foraneo_final" min="0" max="100" value="{{$jerarquia->valor_hasta}}" required><br> 
                 </div>
             </div>
                        
@@ -139,3 +139,78 @@
 </form>
 
 @endsection
+
+<!--Incluimos el modal para validar los campos -->
+
+@include('modales.validarFormularioFormulas')
+
+@push('js')
+
+<!--Validacion de campos del formulario -->
+
+<script type="text/javascript">
+
+    let formulario_formulas = document.getElementById("actualizar_formulas");
+
+    formulario_formulas.addEventListener("submit", function(event){
+
+        //Detenemos el envio del formulario
+        event.preventDefault();
+        
+        let comodin_formulas = true;
+        let texto_formulas;
+
+        //Obtenemos los valores de cada campo
+        let foraneo_inicial = document.getElementById('foraneo_inicial').value;
+        let foraneo_final = document.getElementById('foraneo_final').value;
+        let intermedio_inicial = document.getElementById('intermedio_inicial').value;
+        let intermedio_final = document.getElementById('intermedio_final').value;
+        let local_inicial = document.getElementById('local_inicial').value;
+        let local_final = document.getElementById('local_final').value;
+
+        if (foraneo_inicial  >= foraneo_final) 
+        {
+            comodin_formulas = false;
+            texto_formulas = "El rango del índice foráneo inicial NO puede ser mayor o igual al rango del índice foráneo final";
+        }
+        else if (intermedio_inicial  >= intermedio_final) 
+        {
+            comodin_formulas = false;
+            texto_formulas ="El rango del índice intermedio inicial NO puede ser mayor o igual al rango del índice intermedio final";
+        } 
+       /* else if (local_inicial  >= local_final) 
+        {
+            comodin_formulas = false;
+            texto_formulas ="El rango del índice local inicial NO puede ser mayor o igual al rango del índice local final";
+        }*/
+
+
+        if (!comodin_formulas)
+        {
+            //Desplegamos el modal
+            $('#modal_validar_formulario_formulas').modal('show');
+
+            //Enviamos los valores recuperados anteriormente a los inputs del modal
+            $('#modal_aviso_formulas').text(texto_formulas);
+        }
+
+        if (comodin_formulas)
+        {
+            this.submit();
+        }
+
+    }, false);
+
+</script>
+
+@endpush
+
+@push('css')
+    <style>
+        input:invalid:required {
+
+            border: 2px solid red;
+        }
+    </style>
+@endpush
+
---
+++ resources/views/modales/validarFormularioFormulas.blade.php
@@ -0,0 +1,18 @@
+<div id="modal_validar_formulario_formulas" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title">Aviso - Fórmulas</h1>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p id="modal_aviso_formulas"></p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
