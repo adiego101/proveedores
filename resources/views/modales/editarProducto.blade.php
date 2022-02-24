0e50dab1f8cb9b650d3ce5b2e2445067a306312c
nsartini66 <nsartini66@gmail.com>
2022-02-24 12:41:37 -0300

    Validaciones modal editar Producto

--- resources/views/modales/editarProducto.blade.php
+++ resources/views/modales/editarProducto.blade.php
@@ -10,7 +10,7 @@
                 <div class="modal-body">
 
                     <label for="modal_producto_elaborado">Producto elaborado:</label><br>
-                    <input list="productos" name="modal_producto_elaborado" id="modal_producto_elaborado"  class="form-control" placeholder="Ingrese o seleccione el producto que produce" />
+                    <input list="productos" name="modal_producto_elaborado" id="modal_producto_elaborado"  class="form-control" placeholder="Ingrese o seleccione el producto que produce" required/>
                     <datalist id="productos">
                         @forelse($productos as $producto)
                             <option value="{{$producto->producto_elaborado}}">
@@ -21,14 +21,14 @@
                     <br>
 
                     <label for="modal_rnpa">RNPA:</label><br>
-                    <input type="text" class="form-control" aria-describedby="basic-addon1" id="modal_rnpa" name="modal_rnpa" placeholder="Ingrese el RNPA" maxlength="8"><br>
+                    <input type="text" class="form-control" aria-describedby="basic-addon1" id="modal_rnpa" name="modal_rnpa" placeholder="Ingrese el RNPA" maxlength="8" required><br>
                  
                     <label for="modal_unidad_producida">Unidad producida:</label><br>
                     <input type="text" onkeypress="return valideKey(event);" class="form-control" aria-describedby="basic-addon1" id="modal_unidad_producida"
-                    name="modal_unidad_producida" placeholder="Ingrese la cantidad de unidades producidas" maxlength="9"><br>
+                    name="modal_unidad_producida" placeholder="Ingrese la cantidad de unidades producidas" maxlength="9" required><br>
 
                     <label for="modal_produccion_total">Capacidad de producción total:</label><br>
-                    <input type="text" onkeypress="return valideKey(event);" class="form-control" aria-describedby="basic-addon1" id="modal_produccion_total" name="modal_produccion_total" placeholder="Ingrese la producción total" maxlength="9"><br>
+                    <input type="text" onkeypress="return valideKey(event);" class="form-control" aria-describedby="basic-addon1" id="modal_produccion_total" name="modal_produccion_total" placeholder="Ingrese la producción total" maxlength="9" required><br>
                        
                     <div class="modal-footer">
                         <input id="numero_fila_producto" name="numero_fila_producto" type="hidden">
@@ -43,33 +43,41 @@
 
 <script type="text/javascript">
 
-    //Modificamos los valores actuales, por los nuevos valores ingresados en el modal
+//Modificamos los valores actuales, por los nuevos valores ingresados en el modal
 
-    $(document).on("click", ".btn_edit_modal", function() {
+$(document).on("click", ".btn_edit_modal", function(event) {
 
-                //Obtenemos el numero de la fila que queremos modificar
-                var id_fila = $("#numero_fila_producto").val();
+    //Obtenemos el numero de la fila que queremos modificar
+    var id_fila = $("#numero_fila_producto").val();
 
-                //Recuperamos los valores de los campos del modal
-                var modal_producto_elaborado = $("#modal_producto_elaborado").val();
-                var modal_rnpa = $("#modal_rnpa").val();
-                var modal_unidad_producida = $("#modal_unidad_producida").val();
-                var modal_produccion_total = $("#modal_produccion_total").val();
+    //Recuperamos los valores de los campos del modal
+    var modal_producto_elaborado = $("#modal_producto_elaborado").val();
+    var modal_rnpa = $("#modal_rnpa").val();
+    var modal_unidad_producida = $("#modal_unidad_producida").val();
+    var modal_produccion_total = $("#modal_produccion_total").val();
 
-                //Ocultamos el modal
-                $('#modal_producto').modal('hide');
+    //Si los campos obligatorios NO estan vacios, permite enviar los nuevos valores a la tabla
+    if(modal_producto_elaborado.length != 0 && modal_rnpa.length != 0 && modal_unidad_producida.length != 0 && modal_produccion_total.length != 0){
 
-                //Enviamos los valores recuperados anteriormente del modal, a los inputs de la tabla
-                $('#producto_elaborado'+id_fila).val(modal_producto_elaborado);
-                $('#rnpa'+id_fila).val(modal_rnpa);
-                $('#unidad_producida'+id_fila).val(modal_unidad_producida);
-                $('#produccion_total'+id_fila).val(modal_produccion_total);
+        //Ocultamos el modal
+        $('#modal_producto').modal('hide');
 
-                //Enviamos los valores recuperados anteriormente del modal, a los textos visibles de la tabla
-                $('#producto_elaborado_text'+id_fila).text(modal_producto_elaborado);
-                $('#rnpa_text'+id_fila).text(modal_rnpa);
-                $('#unidad_producida_text'+id_fila).text(modal_unidad_producida);
-                $('#produccion_total_text'+id_fila).text(modal_produccion_total);
+        //Enviamos los valores recuperados anteriormente del modal, a los inputs de la tabla
+        $('#producto_elaborado'+id_fila).val(modal_producto_elaborado);
+        $('#rnpa'+id_fila).val(modal_rnpa);
+        $('#unidad_producida'+id_fila).val(modal_unidad_producida);
+        $('#produccion_total'+id_fila).val(modal_produccion_total);
 
-            });
+        //Enviamos los valores recuperados anteriormente del modal, a los textos visibles de la tabla
+        $('#producto_elaborado_text'+id_fila).text(modal_producto_elaborado);
+        $('#rnpa_text'+id_fila).text(modal_rnpa);
+        $('#unidad_producida_text'+id_fila).text(modal_unidad_producida);
+        $('#produccion_total_text'+id_fila).text(modal_produccion_total);
+
+    }else{
+        //Si alguno de los campos obligatorios esta vacio, detenemos el envio de los datos.
+        event.preventDefault();
+    }
+
+});
 </script>
