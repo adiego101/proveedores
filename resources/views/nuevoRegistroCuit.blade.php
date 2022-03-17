0657786911d03918be212058620152c2dc697376
nsartini66 <nsartini66@gmail.com>
2022-03-17 14:16:20 -0300

    Cambios en la GUI nuevo registro cuit

--- resources/views/nuevoRegistroCuit.blade.php
+++ resources/views/nuevoRegistroCuit.blade.php
@@ -11,10 +11,15 @@
     </div>
 @endif
 
-<h2>Cuit del registro a ingresar</h2>
+<h1>Nuevo Registro</h1>
 
 <br>
 
+<div class="alert alert-info" role="alert">
+    Ingrese el número de CUIT del registro que desea agregar.<br>En el caso de que el CUIT no exista, podrá completar el formulario de alta con los datos correspondientes.<br>En el caso de que el CUIT ya exista será redirigido a un formulario para editar o cargar nuevamente los datos.
+</div>
+
+<br>
 
 <form id="crear_registro_cuit" action="{{ route('crear_registro_cuit') }}"  method="POST">
 @csrf
@@ -23,15 +28,15 @@
 
             <div class="col-sm">
 
+                <label for="cuit">CUIT: <sup>*</sup></label><br>
+                <input type="text" class="form-control" onkeypress="return valideKey(event);" placeholder="Ingrese el número de cuit de la empresa (sin guiones medios)" aria-describedby="basic-addon1" id="cuit" name="cuit" maxlength="11" required><br>
 
-                <label for="cuit">CUIT:</label><br>
-                <input type="text" class="form-control" placeholder="Ingrese CUIT del nuevo registro" aria-describedby="basic-addon1" id="cuit" name="cuit" maxlength="13"  pattern="^[0-9]*(\.?)[0-9]+$" required><br>
             </div>
 
     </div>
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
         <div class="btn-group">
-            <button type="submit" name="Siguiente" class="btn btn-success"> {{ 'Siguiente' }} </button>
+            <button type="submit" name="Siguiente" class="btn btn-primary"> {{ 'Siguiente' }} </button>
         </div>
     </div>
 </div>
@@ -44,17 +49,39 @@
 
 @push('js')
 
+<script type="text/javascript">
+    function valideKey(evt){
 
+        // El código es la representación decimal ASCII de la clave presionada.
+        var code = (evt.which) ? evt.which : evt.keyCode;
 
+        if(code==8) { // espacio.
+          return true;
+        } else if(code>=48 && code<=57) { // es un numero.
+          return true;
+        } else{ // otras teclas
+        console.log("no es un numero");
+          return false;
+        }
+    }
+</script>
 
 @endpush
 
+
 @push('css')
+
     <style>
         input:invalid:required {
 
-            border: 2px solid red;
+            border: 1px dashed red;
+        }
+
+        input:hover:invalid:required {
+
+            background-color: rgb(255, 76, 76);
         }
     </style>
+
 @endpush
 
