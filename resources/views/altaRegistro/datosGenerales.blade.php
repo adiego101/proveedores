4cf62a7b49df318144fcfa4be88c409f3c080c8a
nsartini66 <nsartini66@gmail.com>
2022-03-21 11:20:57 -0300

    Cambio de ubicacion - observaciones

--- resources/views/altaRegistro/datosGenerales.blade.php
+++ resources/views/altaRegistro/datosGenerales.blade.php
@@ -5,11 +5,15 @@
     <label for="razon_social">Razón social: <sup>*</sup></label><br>
     <input type="text" class="form-control" placeholder="Ingrese la razón social" aria-describedby="basic-addon1" id="razon_social" name="razon_social" maxlength="50" autofocus required onkeyup="validaInputDatos()"><br>
 
-    <label for="nombre_fantasia">nombre de fantasía: <sup>*</sup></label><br>
+    <label for="nombre_fantasia">Nombre de fantasía: <sup>*</sup></label><br>
     <input type="text" class="form-control" placeholder="Ingrese el nombre de fantasía" aria-describedby="basic-addon1" id="nombre_fantasia" name="nombre_fantasia" maxlength="50" required onkeyup="validaInputDatos()"><br>
 
     <label for="cuit">CUIT: <sup>*</sup></label><br>
     <input type="text" value="{{ isset($cuit) ? $cuit : '' }}" class="form-control" onkeypress="return valideKey(event);" placeholder="Ingrese el número de cuit de la empresa (sin guiones medios)" aria-describedby="basic-addon1" id="cuit" name="cuit" maxlength="11" required onkeyup="validaInputDatos()"><br>
+
+    <label for="observaciones">Observaciones:</label><br>
+    <textarea id="observaciones" name="observaciones" class="form-control" placeholder="Ingrese las observaciones que considere necesarias" maxlength="200"></textarea>
+    <br>
 
     <hr>
 
@@ -25,12 +29,7 @@
     <label for="dni_legal">DNI:</label><br>
     <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el dni del representante legal" aria-describedby="basic-addon1" id="dni_legal" name="dni_legal" maxlength="8">
     <br>
-    <hr>
-
-    <label for="observaciones">Observaciones:</label><br>
-    <textarea id="observaciones" name="observaciones" class="form-control" placeholder="Ingrese las observaciones que considere necesarias" maxlength="200"></textarea>
     <br>
-
     <input type="button" id="next_datos" name="next" class="next btn btn-info" value="Siguiente" disabled/>
 
 </fieldset>
