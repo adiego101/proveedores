27c7a100f13d6faf86121c75770549c59f9b15bd
Andrea Lopez <landreaceci17@gmail.com>
2022-03-23 12:27:33 -0300

    Tarea programada para dar de baja proveedores despues de un año de su fecha_inscripcion

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,9 +29,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
-            DB::table('proveedores')
+            $result = DB::table('proveedores')
                             ->where('fecha_inscripcion','<=',Carbon::now()->subYear())
-                            ->update('dado_de_baja',1);
+                            ->update(['dado_de_baja'=>1]);
         })->everyMinute();
     }
 
