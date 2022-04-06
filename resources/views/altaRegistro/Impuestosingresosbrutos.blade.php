1c3e42a22697f92e4baec17852622afdc6f0c536
nsartini66 <nsartini66@gmail.com>
2022-04-06 14:33:52 -0300

    Finalizacion de validacion campos numericos - alta -Domicilio fiscal -Impuestos ingresos brutos -Pagos -Personal ocupado

--- resources/views/altaRegistro/Impuestosingresosbrutos.blade.php
+++ resources/views/altaRegistro/Impuestosingresosbrutos.blade.php
@@ -5,7 +5,9 @@
   <h4>Ingresos brutos:</h4><br>
 
   <label for="nro_ingresos_brutos">Número de ingresos brutos:</label><br>
-  <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de ingresos brutos" aria-describedby="basic-addon1" id="nro_ingresos_brutos" name="nro_ingresos_brutos" maxlength="9"><br>
+  <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de ingresos brutos" aria-describedby="basic-addon1" id="nro_ingresos_brutos" name="nro_ingresos_brutos" maxlength="9">
+  <small class="small" id="small-nro_ingresos_brutos"></small>
+  <br>
 
   <label for="jurisdiccion">Jurisdicción:</label><br>
   <input type="text" class="form-control" placeholder="Ingrese su jurisdicción" aria-describedby="basic-addon1" id="jurisdiccion" name="jurisdiccion" maxlength="40"><br>
@@ -23,8 +25,10 @@
   <h4>Habilitaciones:</h4><br>
 
   <label for="nro_habilitacion_municipal">Número de habilitación municipal:</label><br>
-  <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de habilitación municipal" aria-describedby="basic-addon1" id="nro_habilitacion_municipal" name="nro_habilitacion_municipal" maxlength="20"><br>
-
+  <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de habilitación municipal" aria-describedby="basic-addon1" id="nro_habilitacion_municipal" name="nro_habilitacion_municipal" maxlength="20">
+  <small class="small" id="small-nro_habilitacion_municipal"></small>
+  <br>
+  
   <div class="container">
     <div class="row">
       <div class="col-sm">
@@ -51,8 +55,9 @@
     <div class="row">
       <div class="col-sm">
         <label for="nro_inscripcion_personas_juridicas">Número de inscripción personas jurídicas:</label><br>
-        <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de inscripción de personas jurídicas" aria-describedby="basic-addon1" id="nro_inscripcion_personas_juridicas" name="nro_inscripcion_personas_juridicas" maxlength="4"><br>
-      </div>
+        <input type="text" onkeypress="return valideKey(event);" class="form-control" placeholder="Ingrese el número de inscripción de personas jurídicas" aria-describedby="basic-addon1" id="nro_inscripcion_personas_juridicas" name="nro_inscripcion_personas_juridicas" maxlength="4">
+        <small class="small" id="small-nro_inscripcion_personas_juridicas"></small>
+    </div>
 
       <div class="col-sm">
         <label for="provincia_inscrip_personas_jur">Provincia:</label><br>
@@ -115,7 +120,49 @@
   <input type="button" name="next" class="next btn btn-info" value="Siguiente" />
 
 
-  <script type="text/javascript">
+<script type="text/javascript">
+
+$('#nro_inscripcion_personas_juridicas').keyup(validarnro_inscripcion_personas_juridicas);
+
+function validarnro_inscripcion_personas_juridicas() {
+
+    if (!(/^[0-9]/.test($('#nro_inscripcion_personas_juridicas').val()))) {
+
+        mostrarError('#nro_inscripcion_personas_juridicas', '#small-nro_inscripcion_personas_juridicas', '<div class="alert alert-danger mt-3 pt-1">El <strong>Número de inscripción personas jurídicas</strong> debe contener solamente dígitos numéricos.</div>');
+        return false;
+    }
+    ocultarError('#nro_inscripcion_personas_juridicas', '#small-nro_inscripcion_personas_juridicas');
+    return true;
+}
+
+$('#nro_ingresos_brutos').keyup(validarnro_ingresos_brutos);
+
+function validarnro_ingresos_brutos() {
+
+    if (!(/^[0-9]/.test($('#nro_ingresos_brutos').val()))) {
+
+        mostrarError('#nro_ingresos_brutos', '#small-nro_ingresos_brutos', '<div class="alert alert-danger mt-3 pt-1">El <strong>número de ingresos brutos</strong> debe contener solamente dígitos numéricos.</div>');
+        return false;
+    }
+    ocultarError('#nro_ingresos_brutos', '#small-nro_ingresos_brutos');
+    return true;
+}
+
+$('#nro_habilitacion_municipal').keyup(validarnro_habilitacion_municipal);
+
+function validarnro_habilitacion_municipal() {
+
+    if (!(/^[0-9]/.test($('#nro_habilitacion_municipal').val()))) {
+
+        mostrarError('#nro_habilitacion_municipal', '#small-nro_habilitacion_municipal', '<div class="alert alert-danger mt-3 pt-1">El <strong>Número de habilitación municipal</strong> debe contener solamente dígitos numéricos.</div>');
+        return false;
+    }
+    ocultarError('#nro_habilitacion_municipal', '#small-nro_habilitacion_municipal');
+    return true;
+}
+
+
+
 	$(document).ready(function(){
 
 		$('#provincia_habilitacion').change(function(){
