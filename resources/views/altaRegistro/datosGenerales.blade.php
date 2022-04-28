44812bd4dc74ec0032dbf4d4d113f7906b623a6d
Nicolas Sartini <nsartini66@gmail.com>
2022-04-28 14:55:52 -0300

    Habilitacion por defecto boton siguiente -Se comento el script que deshabilita el boton, hasta descargarlo local

--- resources/views/altaRegistro/datosGenerales.blade.php
+++ resources/views/altaRegistro/datosGenerales.blade.php
@@ -3,12 +3,13 @@
     <h1>Datos generales</h1><br>
 
     <label for="razon_social">Razón social: <sup>*</sup></label><br>
-    <input type="text" class="form-control" placeholder="Ingrese la razón social" aria-describedby="basic-addon1" id="razon_social" name="razon_social" maxlength="50" autofocus required onkeyup="validaInputDatos()" value="{{old('razon_social')}}"><br>
+    <input type="text" class="form-control" placeholder="Ingrese la razón social" aria-describedby="basic-addon1" id="razon_social" name="razon_social" maxlength="50" autofocus required value="{{old('razon_social')}}"><br>
+
     <label for="nombre_fantasia">Nombre de fantasía: <sup>*</sup></label><br>
-    <input type="text" class="form-control" placeholder="Ingrese el nombre de fantasía" aria-describedby="basic-addon1" id="nombre_fantasia" name="nombre_fantasia" maxlength="50" required onkeyup="validaInputDatos()" value="{{old('nombre_fantasia')}}"><br>
+    <input type="text" class="form-control" placeholder="Ingrese el nombre de fantasía" aria-describedby="basic-addon1" id="nombre_fantasia" name="nombre_fantasia" maxlength="50" required value="{{old('nombre_fantasia')}}"><br>
 
     <label for="cuit">CUIT: <sup>*</sup></label><br>
-    <input type="text" value="{{ isset($cuit) ? $cuit : '' }}" class="form-control" onkeypress="return valideKey(event);" placeholder="Ingrese el número de cuit de la empresa (sin guiones medios)" aria-describedby="basic-addon1" id="cuit" name="cuit" maxlength="11" required onkeyup="validaInputDatos()">
+    <input type="text" value="{{ isset($cuit) ? $cuit : '' }}" class="form-control" onkeypress="return valideKey(event);" placeholder="Ingrese el número de cuit de la empresa (sin guiones medios)" aria-describedby="basic-addon1" id="cuit" name="cuit" maxlength="11" required>
     <small class="small" id="small-cuit"></small>
 
     <br>
@@ -40,13 +41,16 @@
 
 @push('js')
 <script type="text/javascript">
-
+    
+/*
     $("#document").ready(function(){
         if($("#razon_social").val()!='' && $("#nombre_fantasia").val() && $("#cuit").val())
             $("#next_datos").removeAttr("disabled");
         else
             $("#next_datos").attr("disabled", "disabled");
     });
+*/
+
     $('#cuit').keyup(validarNumerocuit);
 
     function validarNumerocuit() {
@@ -77,8 +81,9 @@
         return true;
     }
 
-
-
+//DESCOMENTAR UNA VEZ QUE SE DESCARGUEN LAS LIBRERIAS LOCALMENTE EN EL SERVIDOR.
+//Esta funcion habilita o deshabilita el boton siguiente segun el completado de los campos obligatorios
+/*
     let boton_siguiente = document.getElementById("next_datos");
     let razon_social_datos = document.getElementById("razon_social");
     let nombre_fantasia_datos = document.getElementById("nombre_fantasia");
@@ -97,6 +102,16 @@
         }
 
     }
+*/
+
+/*Ademas se debe agregar el siguiente atributo a los inputs:
+
+    -Razon social
+    -Nombre de fantasia
+    -CUIT
+
+    onkeyup="validaInputDatos()"
+*/
 
 </script>
 
